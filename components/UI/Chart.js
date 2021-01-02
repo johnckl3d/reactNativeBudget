0294937f18555d8c7ed1e1ca48bdb916417a2c54
John Cheang <johnckl3d@gmail.com>
2021-01-02 22:56:09 +0800

    normalization complete

--- components/UI/Chart.js
+++ components/UI/Chart.js
@@ -21,10 +21,13 @@ import * as shape from "d3-shape";
 import { scaleTime, scaleLinear, scaleQuantile } from "d3-scale";
 import Moment from "moment";
 import { dragDisable } from "d3";
-import { getWeekOfDayWithOffset, getFirstDayOfWeek } from "../../helpers/helpers";
+import {
+  getWeekOfDayWithOffset,
+  getFirstDayOfWeek,
+} from "../../helpers/helpers";
 import CostSnapShot from "../../models/costSnapShot";
-import dateFns from 'date-fns';
-import {format} from "date-fns/format";
+import dateFns from "date-fns";
+import { format } from "date-fns/format";
 
 const height = 200;
 const width = Dimensions.get("window").width * 0.9;
@@ -87,8 +90,14 @@ export default class Chart extends Component {
 
   normalizeChart(rawData) {
     console.log("raw:" + JSON.stringify(rawData));
-    var arr = [{x:Moment(), y:0}, {x:Moment(), y:0}, {x:Moment(), y:0}, {x:Moment(), y:0}, {x:Moment(), y:0}];
-    
+    var arr = [
+      { x: Moment(), y: 0 },
+      { x: Moment(), y: 0 },
+      { x: Moment(), y: 0 },
+      { x: Moment(), y: 0 },
+      { x: Moment(), y: 0 },
+    ];
+
     rawData.forEach((input) => {
       const week = getWeekOfDayWithOffset(Moment(input.x));
       const amount = Number(input.y);
@@ -97,8 +106,6 @@ export default class Chart extends Component {
       const firstDayOfWeek = getFirstDayOfWeek(Moment(input.x));
       arr[week - 1].x = firstDayOfWeek;
     });
-
-    
 
     // const arrXY = arr.map((snapshot) => ({
     //   x: new Date(snapshot.dateTime),
@@ -160,92 +167,91 @@ export default class Chart extends Component {
       console.log("width:" + JSON.stringify(width));
       console.log("height:" + JSON.stringify(height));
 
-      const yAxesSvg = { fontSize: 12, fill: "black" };
+      const yAxesSvg = { fill: "black", fontSize: 8, fontWeight: "bold" };
+
       const verticalContentInset = { left: 10, right: 10, top: 10, bottom: 10 };
 
       return (
-        <View style={{ flexDirection: "row", borderWidth: 1 }}>
-          <View style={{ borderWidth: 1, flex: 1 }}>
-          <YAxis
-          style={{ marginHorizontal: -10, height: height }}
-            data={data}
-            yAccessor={({ item }) => item.y}
-            contentInset={verticalContentInset}
-            svg={yAxesSvg}
-            formatLabel={(value) => 
-              '' + value
-            }
-          />
+        <View style={{ flexDirection: "row" }}>
+          <View style={{ flex: 1 }}>
+            <YAxis
+              style={{ marginHorizontal: -10, height: height }}
+              data={data}
+              yAccessor={({ item }) => item.y}
+              contentInset={verticalContentInset}
+              svg={yAxesSvg}
+              formatLabel={(value) => "" + value}
+            />
           </View>
           <View style={{ flex: 9 }}>
-            <Svg {...{ width, height }}>
-              <Defs>
-                <LinearGradient
-                  x1="50%"
-                  y1="0%"
-                  x2="50%"
-                  y2="100%"
-                  id="gradient"
-                >
-                  <Stop stopColor="#CDE3F8" offset="0%" />
-                  <Stop stopColor="#eef6fd" offset="80%" />
-                  <Stop stopColor="#FEFFFF" offset="100%" />
-                </LinearGradient>
-              </Defs>
-              <Path
-                d={line}
-                fill="transparent"
-                stroke="#367be2"
-                strokeWidth={5}
-              />
-              <Path
-                d={`${line} L ${width} ${height} L 0 ${height}`}
-                fill="url(#gradient)"
-              />
-              <View ref={this.cursor} style={styles.cursor} />
-            </Svg>
-            <Animated.View
-              style={[styles.label, { transform: [{ translateX }] }]}
-            >
-              <TextInput ref={this.label} />
-            </Animated.View>
-            <Animated.ScrollView
-              style={StyleSheet.absoluteFill}
-              contentContainerStyle={{ width: lineLength * 2 }}
-              showsHorizontalScrollIndicator={false}
-              scrollEventThrottle={16}
-              bounces={false}
-              onScroll={Animated.event(
-                [
-                  {
-                    nativeEvent: {
-                      contentOffset: { x },
-                    },
-                  },
-                ],
-                { useNativeDriver: true }
-              )}
-              horizontal
-            />
-             <XAxis
-                    data={ data }
-                    svg={{
-                        fill: 'black',
-                        fontSize: 8,
-                        fontWeight: 'bold',
-                        rotation: 20,
-                        originY: 30,
-                        y: 5,
-                    }}
-                    xAccessor={ ({ item }) => item.x }
-                    scale={ scaleTime }
-                    numberOfTicks={ 6 }
-                    style={{ marginHorizontal: -15, height: 20 }}
-                    contentInset={{ left: 10, right: 25 }}
-                    formatLabel={ (index) => 
-                      Moment(index).format('D')}
-                    
+            <View style={{ borderLeftWidth: 1, borderBottomWidth: 1 }}>
+              <Svg {...{ width, height }}>
+                <Defs>
+                  <LinearGradient
+                    x1="50%"
+                    y1="0%"
+                    x2="50%"
+                    y2="100%"
+                    id="gradient"
+                  >
+                    <Stop stopColor="#CDE3F8" offset="0%" />
+                    <Stop stopColor="#eef6fd" offset="80%" />
+                    <Stop stopColor="#FEFFFF" offset="100%" />
+                  </LinearGradient>
+                </Defs>
+                <Path
+                  d={line}
+                  fill="transparent"
+                  stroke="#367be2"
+                  strokeWidth={5}
                 />
+                <Path
+                  d={`${line} L ${width} ${height} L 0 ${height}`}
+                  fill="url(#gradient)"
+                />
+                <View ref={this.cursor} style={styles.cursor} />
+              </Svg>
+              <Animated.View
+                style={[styles.label, { transform: [{ translateX }] }]}
+              >
+                <TextInput ref={this.label} />
+              </Animated.View>
+              <Animated.ScrollView
+                style={StyleSheet.absoluteFill}
+                contentContainerStyle={{ width: lineLength * 2 }}
+                showsHorizontalScrollIndicator={false}
+                scrollEventThrottle={16}
+                bounces={false}
+                onScroll={Animated.event(
+                  [
+                    {
+                      nativeEvent: {
+                        contentOffset: { x },
+                      },
+                    },
+                  ],
+                  { useNativeDriver: true }
+                )}
+                horizontal
+              />
+            </View>
+            <XAxis
+              data={data}
+              svg={{
+                fill: "black",
+                fontSize: 8,
+                fontWeight: "bold",
+                rotation: 20,
+                originY: 30,
+                y: 5,
+              }}
+              xAccessor={({ item }) => item.x}
+              scale={scaleTime}
+              numberOfTicks={6}
+              style={{ marginHorizontal: -15, height: 20 }}
+              contentInset={{ left: 10, right: 25 }}
+              formatLabel={(index) => Moment(index).format("D")}
+            />
           </View>
         </View>
       );
