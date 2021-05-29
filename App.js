2453a20430f5e4610f3a3fa171deb27773d65987
John Cheang <johnckl3d@gmail.com>
2021-05-29 17:38:31 +0800

    before using paper

--- App.js
+++ App.js
@@ -1,16 +1,26 @@
-import React, { useState } from 'react';
-import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
-import { Provider } from 'react-redux';
-import { AppLoading } from 'expo';
-import * as Font from 'expo-font';
-import ReduxThunk from 'redux-thunk';
+import React, { useState } from "react";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import { DefaultTheme, Provider as PaperProvider } from "react-native-paper";
+import { Provider } from "react-redux";
+import { AppLoading } from "expo";
+import * as Font from "expo-font";
+import ReduxThunk from "redux-thunk";
 
-import productsReducer from './store/reducers/products';
-import cartReducer from './store/reducers/cart';
-import ordersReducer from './store/reducers/orders';
-import budgetsReducer from './store/reducers/budgets';
-import costCategoriesReducer from './store/reducers/costCategories';
-import ShopNavigator from './navigation/ShopNavigator';
+import productsReducer from "./store/reducers/products";
+import cartReducer from "./store/reducers/cart";
+import ordersReducer from "./store/reducers/orders";
+import budgetsReducer from "./store/reducers/budgets";
+import costCategoriesReducer from "./store/reducers/costCategories";
+import ShopNavigator from "./navigation/ShopNavigator";
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: 'tomato',
+    accent: 'yellow',
+  },
+};
 
 const rootReducer = combineReducers({
   products: productsReducer,
@@ -21,21 +31,24 @@ const rootReducer = combineReducers({
 });
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(ReduxThunk)));
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(ReduxThunk))
+);
 
 const fetchFonts = () => {
   return Font.loadAsync({
-    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
-    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
-    'open-sans-boldItalic': require('./assets/fonts/OpenSans-BoldItalic.ttf'),
-    'open-sans-extraBold': require('./assets/fonts/OpenSans-ExtraBold.ttf'),
-    'open-sans-extraBoldItalic': require('./assets/fonts/OpenSans-ExtraBoldItalic.ttf'),
-    'open-sans-italic': require('./assets/fonts/OpenSans-Italic.ttf'),
-    'open-sans-light': require('./assets/fonts/OpenSans-Light.ttf'),
-    'open-sans-lightItalic': require('./assets/fonts/OpenSans-LightItalic.ttf'),
-    'open-sans-regular': require('./assets/fonts/OpenSans-Regular.ttf'),
-    'open-sans-semiBold': require('./assets/fonts/OpenSans-SemiBold.ttf'),
-    'open-sans-semiBoldItalic': require('./assets/fonts/OpenSans-SemiBoldItalic.ttf')
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+    "open-sans-boldItalic": require("./assets/fonts/OpenSans-BoldItalic.ttf"),
+    "open-sans-extraBold": require("./assets/fonts/OpenSans-ExtraBold.ttf"),
+    "open-sans-extraBoldItalic": require("./assets/fonts/OpenSans-ExtraBoldItalic.ttf"),
+    "open-sans-italic": require("./assets/fonts/OpenSans-Italic.ttf"),
+    "open-sans-light": require("./assets/fonts/OpenSans-Light.ttf"),
+    "open-sans-lightItalic": require("./assets/fonts/OpenSans-LightItalic.ttf"),
+    "open-sans-regular": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-semiBold": require("./assets/fonts/OpenSans-SemiBold.ttf"),
+    "open-sans-semiBoldItalic": require("./assets/fonts/OpenSans-SemiBoldItalic.ttf"),
   });
 };
 
@@ -54,7 +67,9 @@ export default function App() {
   }
   return (
     <Provider store={store}>
-      <ShopNavigator />
+      <PaperProvider>
+        <ShopNavigator />
+      </PaperProvider>
     </Provider>
   );
 }
